60d784ba9fa558d884de92abd4a0b45311add3c7
dnivra26 <dnivra26@gmail.com>
2015-07-19 04:14:05 +0530

    Change primary to color to white.

--- app/src/main/java/io/github/dnivra26/cleansweep/MainActivity.java
+++ app/src/main/java/io/github/dnivra26/cleansweep/MainActivity.java
@@ -66,9 +66,6 @@ public class MainActivity extends AppCompatActivity
             case 2:
                 mTitle = getString(R.string.title_section2);
                 break;
-            case 3:
-                mTitle = getString(R.string.title_section3);
-                break;
         }
     }
 

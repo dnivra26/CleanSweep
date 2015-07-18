24cf23635c0e1978bd7aa8ca20470860dc8f1e0a
dnivra26 <dnivra26@gmail.com>
2015-07-18 16:13:25 +0530

    Added signup functionality

--- app/src/main/java/io/github/dnivra26/cleansweep/LoginActivity.java
+++ app/src/main/java/io/github/dnivra26/cleansweep/LoginActivity.java
@@ -5,14 +5,55 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.parse.ParseAnonymousUtils;
+import com.parse.ParseException;
 import com.parse.ParseUser;
 
+import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
 
 @EActivity(R.layout.activity_login)
 public class LoginActivity extends AppCompatActivity {
+
+    @ViewById(R.id.username)
+    EditText usernameEditText;
+
+    @ViewById(R.id.password)
+    EditText passwordEditText;
+
+
+    @Click(R.id.signup)
+    public void signUp() {
+        String username = usernameEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
+
+        if (username.equals("") && password.equals("")) {
+            Toast.makeText(getApplicationContext(),
+                    "Please complete the sign up form",
+                    Toast.LENGTH_LONG).show();
+
+        } else {
+            ParseUser user = new ParseUser();
+
+            user.setUsername(username);
+            user.setPassword(password);
+            try {
+                user.signUp();
+                Toast.makeText(getApplicationContext(),
+                        "Successfully Signed up, please log in.",
+                        Toast.LENGTH_LONG).show();
+            } catch (ParseException e) {
+                Toast.makeText(getApplicationContext(),
+                        "Sign up Error", Toast.LENGTH_LONG)
+                        .show();
+                e.printStackTrace();
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

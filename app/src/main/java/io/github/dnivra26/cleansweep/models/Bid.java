7f0a0528b3b8df6a1284bce08d5274ac8c1ae6b5
GanesshKumarRP <ganessh@nomadly.in>
2015-07-18 17:13:49 +0530

    Models

--- app/src/main/java/io/github/dnivra26/cleansweep/models/Bid.java
+++ app/src/main/java/io/github/dnivra26/cleansweep/models/Bid.java
@@ -1,8 +1,16 @@
 package io.github.dnivra26.cleansweep.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * Created by ganesshkumar on 18/07/15.
  */
+
+@Getter
+@Setter
+@AllArgsConstructor(suppressConstructorProperties = true)
 public class Bid {
     private long bidId;
     private long issueId;
--- app/src/main/java/io/github/dnivra26/cleansweep/models/Issue.java
+++ app/src/main/java/io/github/dnivra26/cleansweep/models/Issue.java
@@ -3,5 +3,20 @@ package io.github.dnivra26.cleansweep.models;
 /**
  * Created by ganesshkumar on 18/07/15.
  */
+
+import android.graphics.Bitmap;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor(suppressConstructorProperties = true)
 public class Issue {
+    private long issueId;
+    private String title;
+    private String location;
+    private Bitmap photo;
+    private float bid;
 }

539a090279e615f7e33a734f65ff5ca8d3af16fd
GanesshKumarRP <ganessh@nomadly.in>
2015-07-18 17:12:28 +0530

    models

---
+++ app/src/main/java/io/github/dnivra26/cleansweep/models/Bid.java
@@ -0,0 +1,11 @@
+package io.github.dnivra26.cleansweep.models;
+
+/**
+ * Created by ganesshkumar on 18/07/15.
+ */
+public class Bid {
+    private long bidId;
+    private long issueId;
+    private String username;
+    private float amount;
+}
---
+++ app/src/main/java/io/github/dnivra26/cleansweep/models/Issue.java
@@ -0,0 +1,7 @@
+package io.github.dnivra26.cleansweep.models;
+
+/**
+ * Created by ganesshkumar on 18/07/15.
+ */
+public class Issue {
+}

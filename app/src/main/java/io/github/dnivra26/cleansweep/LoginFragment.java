835e46322393dc7ad06ee71816537e26c69c70ba
dnivra26 <dnivra26@gmail.com>
2015-07-19 07:25:43 +0530

    remove openening navigation drawer by default. add progress bar to initial load

--- app/src/main/java/io/github/dnivra26/cleansweep/LoginFragment.java
+++ app/src/main/java/io/github/dnivra26/cleansweep/LoginFragment.java
@@ -2,15 +2,20 @@ package io.github.dnivra26.cleansweep;
 
 
 import android.app.Fragment;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
 import android.widget.ListView;
+
+import com.parse.ParseQueryAdapter;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EFragment;
 import org.androidannotations.annotations.ItemClick;
 import org.androidannotations.annotations.ViewById;
+
+import java.util.List;
 
 import io.github.dnivra26.cleansweep.models.Issue;
 
@@ -30,7 +35,6 @@ public class LoginFragment extends Fragment {
 
 
     public LoginFragment() {
-        // Required empty public constructor
     }
 
     public static LoginFragment newInstance() {
@@ -51,8 +55,19 @@ public class LoginFragment extends Fragment {
     public void init() {
         ((MainActivity) getActivity()).getSupportActionBar().setTitle("Issue List");
         IssueListAdapter issueListAdapter = new IssueListAdapter(getActivity());
+        final ProgressDialog progressDialog = UiUtil.buildProgressDialog(getActivity());
+        issueListAdapter.addOnQueryLoadListener(new ParseQueryAdapter.OnQueryLoadListener<Issue>() {
+            @Override
+            public void onLoading() {
+                progressDialog.show();
+            }
+
+            @Override
+            public void onLoaded(List<Issue> list, Exception e) {
+                progressDialog.dismiss();
+            }
+        });
         issueList.setAdapter(issueListAdapter);
-        issueListAdapter.notifyDataSetChanged();
     }
 
 }
